ab3e4025b11fd9b4f307ef47a39de917b2df44f6
Andrew Huang <huanga2@student.unimelb.edu.au>
2020-03-23 18:45:38 +1100

    proof of concept

--- Test.py
+++ Test.py
@@ -3,6 +3,7 @@
 import glob
 import math
 import re
+from typing import Tuple
 
 from pprint import pprint
 
@@ -10,27 +11,36 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def processImage(imageFile: str):
+def processImage(imageFile: str, preview: bool = 0) -> Tuple[np.ndarray, np.ndarray]:
+    global pattern_size
+
+    if not preview:
+        print('#', end='')
+
     img = cv2.imread(imageFile)
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, (11, 7), None)
+    ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
 
     if ret:
-        for i, corner in enumerate(corners):
-            x, y = corner.ravel()
-            cv2.circle(img, (x, y), 10, (0, 0, i/(11*7) * 255), -1)
+        if preview:
+            cv2.destroyAllWindows()
+            for i, corner in enumerate(corners):
+                x, y = corner.ravel()
+                cv2.circle(img, (x, y), 10, (0, 0, i/(11*7) * 255), -1)
+            cv2.imshow(imageFile, img)
+            cv2.waitKey(100)
 
         return (pattern_points, corners)
 
-ImageFiles = glob.glob("./CalibImages/Left/*.png")
+global pattern_size
+
+ImageFiles = glob.glob("./CalibImages/Left/*.png")[::3]
 
 square_size = 20
 pattern_size = (11, 7)
-pattern_points = np.zeros((11*7, 3), np.float32)
+pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
 pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
-pattern_points[:, 0] -= 5
-pattern_points[:, 1] = pattern_size[1] - pattern_points[:, 1]
 pattern_points *= square_size
 
 # Arrays to store object points and image points from all the images.
@@ -42,19 +52,23 @@ imageSize = gray.shape[::-1]
 
 chessboards = []
 
-threads_num = 4
+threads_num = 12
 if threads_num <= 1:
-    chessboards = [processImage(fn) for fn in ImageFiles]
+    chessboards = [processImage(fn, 1) for fn in ImageFiles]
 else:
     print("Run with %d threads..." % threads_num)
+    print('Processing Images:\n' + '#'*len(ImageFiles))
     from multiprocessing.dummy import Pool as ThreadPool
     pool = ThreadPool(threads_num)
     chessboards = pool.map(processImage, ImageFiles)
+
+chessboards = [i for i in chessboards if i is not None]
 
 for (pattern_points, corners) in chessboards:
     objpoints.append(pattern_points)
     imgpoints.append(corners)
 
+print('\nCalibrating ...')
 rms, camera_matrix, dist_coefs, _rvecs, _tvecs = cv2.calibrateCamera(objpoints, imgpoints, imageSize, None, None)
 
 print("\nRMS:", rms)
@@ -62,10 +76,10 @@ print("Num images: ", len(imgpoints))
 print("camera matrix:\n", camera_matrix)
 print("distortion coefficients: ", dist_coefs.ravel())
 
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, imageSize, 1, imageSize)
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, imageSize, 0, imageSize)
 
-# undistort
-testImage = cv2.imread('./Left_0.png')
+# Undistort Test Image
+testImage = cv2.imread('./Left_1.png')
 dst = cv2.undistort(testImage, camera_matrix, dist_coefs, None, newcameramtx)
 cv2.namedWindow('undist', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('undist', 960, 720)
